1cd68bd9d346728c57d87b81bef2effe82ffab5c
131 <a131@131s-MacBook-Air.local>
2022-12-26 08:43:16 +0530

    fixes

--- sftp-backend/read.js
+++ sftp-backend/read.js
@@ -1,20 +1,23 @@
 let express = require("express");
 let router = express.Router();
 var browseDir = require("browse-directory");
+let Moment =require("moment");
 var tree1 = browseDir.browse("node_modules");
 var tree2 = browseDir.browseFiles("node_modules");
 var tree3 = browseDir.browseDirs("node_modules");
-
-console.log("saddsaadssad1", browseDir.showTree(tree1));
-console.log("saddsaadssad2", tree2);
-console.log("saddsaadssad3", tree3);
+const dirTree = require("directory-tree");
+const tree = dirTree("../../Learning");
+console.log("asdasdsasasad",tree)
+// console.log("saddsaadssad1", browseDir.showTree(tree1));
+// console.log("saddsaadssad2", tree2);
+// console.log("saddsaadssad3", tree3);
 
 const listDirectory = (req, res) => {
     let resp=[]
     for (let i = 0; i < tree2.length; i++) {
         resp.push({
             key: tree2[i].src,
-            modified: "",
+            modified: +Moment().subtract(1, "hours"),
             size: 1.5 * 1024 * 1024,
         })
         
