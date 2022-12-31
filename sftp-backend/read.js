16a078fb38b77541d25397c0980efd2612a055dc
131 <a131@131s-MacBook-Air.local>
2022-12-31 11:07:29 +0530

    updated

--- sftp-backend/read.js
+++ sftp-backend/read.js
@@ -1,4 +1,5 @@
 let express = require("express");
+var path = require('path');
 let router = express.Router();
 var browseDir = require("browse-directory");
 let Moment = require("moment");
@@ -7,15 +8,17 @@ var tree2 = browseDir.browseFiles("node_modules");
 var tree3 = browseDir.browseDirs("node_modules");
 const dirTree = require("directory-tree");
 const tree = dirTree("../../Learning/wedding-invite");
-// console.log("asdasdsasasad", JSON.stringify(tree));
-// console.log("asdasdsasasad", tree.children[1].children[5].children);
-// console.log("saddsaadssad1", browseDir.showTree(tree1));
-// console.log("saddsaadssad2", tree2);
-// console.log("saddsaadssad3", tree3);
+
 let paths = [];
+let ext = ["\\.png", "\\.mp3", "\\.mp4"];
 const processChildren = (array) => {
   for (let i = 0; i < array.length; i++) {
-    paths.push(array[i].path);
+    ext.map((e) => {
+      if (array[i].path.search(e) !== -1) {
+        paths.push(array[i].path);
+      }
+    });
+
     if (array[i].children !== undefined) {
       if (array[i].children.length !== 0) {
         processChildren(array[i].children);
@@ -23,54 +26,45 @@ const processChildren = (array) => {
     }
   }
 };
-const removeDuplicatePaths = (pathArr) => {
-  let rr = [];
-  for (let i = 0; i < pathArr.length; i++) {
-    for (let j = 0; j < pathArr.length; j++) {
-      // console.log("ASDadsdasdasdasdasdas",pathArr[j].search(pathArr[i]))
-      if (pathArr[j].search(pathArr[i]) == -1) {
-
-        if (pathArr[j].length > pathArr[i].length) {
-          rr.push(pathArr[j]);
-        } else {
-          rr.push(pathArr[i]);
-        }
-      }
-    }
-  }
-  return rr
-};
 
 const listDirectory = (req, res) => {
   let resp = [];
-  for (let i = 0; i < tree2.length; i++) {
-    resp.push({
-      key: tree2[i].src,
-      modified: +Moment().subtract(1, "hours"),
-      size: 1.5 * 1024 * 1024,
-    });
-  }
   let dirs = tree.children;
   processChildren(dirs);
   console.log("sdasadadsdas", paths);
-  // console.log("asdasdsad",removeDuplicatePaths(paths))
-  let rrrr= removeDuplicatePaths(paths)
-  for (let i = 0; i < rrrr.length; i++) {
-    // if (dirs[i].children.length !== 0) {
-    //   processChildren(dirs)
-    // }
+  for (let i = 0; i < paths.length; i++) {
     resp.push({
-      key: rrrr[i].slice(5),
+      key: paths[i].slice(5),
       modified: +Moment().subtract(1, "hours"),
       size: 1.5 * 1024 * 1024,
     });
   }
-
+  console.log("ssssss", paths);
+  paths = [];
   return res.status(200).json({
     dirs: resp,
   });
 };
 
+const downloadFile = (req, res) => {
+  console.log("sdsdsdsdasdasdasda", req.body);
+  let options = {
+    root: path.join("../../"),
+  };
+  return res.sendFile(path.resolve(`${req.body.filePath}`), options, function (err) {
+    if (err) {
+   console.log("err",err)
+    } else {
+      console.log("Sent:", 'app.js');
+    }
+  });
+
+  // return res.status(200).json({
+  //   message: "SUCCESS",
+  // });
+};
+
 router.get("/list", listDirectory);
+router.post("/download", downloadFile);
 
 module.exports = router;
